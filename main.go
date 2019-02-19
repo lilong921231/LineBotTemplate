792486c2ca07bbbaf7b617e2b81a448c7eb69f20
lilong <568639606@qq.com>
2019-02-19 11:49:33 +0800

    ceshi

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" lilong")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" 在测试")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
